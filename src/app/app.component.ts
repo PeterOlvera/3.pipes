0c6dc5d6387a509a760ef1587cab75ce45127bb6
Olvera Quisilay <r.olvera.quisilay@accenture.com>
2017-07-19 13:32:23 +0200

    pipes hasta personalizados, error de compilacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = "Peter ";
+  PI = Math.PI;
+
+  a:number = 0.234;
+
+  salario = 1234.5;
+
+  xmen={
+    nombre:"Logan",
+    clave:"wolverine",
+    edad:500,
+    direccion:{
+      calle:"primera",
+      casa:"19"
+    }
+  }
+
+
+  valorDePromesa = new Promise( (resolve, reject)=>{
+    setTimeout( ()=>resolve('llegó la data!'),3500 );
+  } );
+
+  fecha = new Date();
+
+  nombreMal:string = "PeTeR olVera quiSILAY";
+
 }
